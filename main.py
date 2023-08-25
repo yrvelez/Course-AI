eaca576868f12ac9f53ecd3bac441fae8659129b
yrvelez <52504034+yrvelez@users.noreply.github.com>
2023-08-25 14:04:19 +0000

    Created an expandable list of sources

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ import weaviate
 app = Flask(__name__)
 documents = SimpleDirectoryReader('materials').load_data()
 index = GPTVectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine(similarity_top_k=3,
+query_engine = index.as_query_engine(similarity_top_k=5,
                                      response_mode='tree_summarize')
-
 
 @app.route('/')
 def home():
   return render_template('index.html')
 
-
 @app.route('/chat', methods=['POST'])
 def chat():
   user_input = request.form['user_input']
   response = query_engine.query(user_input)
-  sources = ', '.join([
-    f'{dict(dict(response.source_nodes[i])["node"])["metadata"]["file_name"].replace(".pdf", "").replace("_", " ").title()} (pg. {dict(dict(response.source_nodes[i])["node"])["metadata"]["page_label"]})'
+  sources_data = [
+    {
+        'source': f'{dict(dict(response.source_nodes[i])["node"])["metadata"]["file_name"].replace(".pdf", "").replace("_", " ").title()} (pg. {dict(dict(response.source_nodes[i])["node"])["metadata"]["page_label"]})',
+        'text': dict(dict(response.source_nodes[i])['node'])['text']
+    }
     for i in range(min(5, len(response.source_nodes)))
-  ])
+  ]
 
-  output = str(response) + '<br><br>Most Relevant Sources: ' + sources
-  return jsonify({'response': output})
+  return jsonify({'response': str(response), 'sources': sources_data})
 
 
 if __name__ == '__main__':
